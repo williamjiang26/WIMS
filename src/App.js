bf7be6a409f25bcdec9c44993235d281b4485a85
William Jiang <williamjiang26@Williams-Air.home>
2023-04-27 18:49:24 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+function NavBar(){
+  return (
+    <nav>
+		<ul>
+			<li><a href="/">Staff Directory</a></li>
+      <li><a href="/staff/add">Add Staff!</a></li>
+			<li><a href="/orders">My Orders</a></li>
+			<li><a href="/inventory">My Inventory</a></li>
+		</ul>
+	</nav>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <NavBar/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
